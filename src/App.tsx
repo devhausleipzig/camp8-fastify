df505530e929333e10fc81f0a64812c43e86d2eb
Franz Wollang <franzwollang@gmail.com>
2023-01-24 16:50:47 +0100

    added pokedex template

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import "./App.css";
 function App() {
 	return (
 		<div className="flex justify-center">
-			<div className="flex flex-col justify-between bg-gray-400 w-[300px] h-[500px]">
-				<h1>Pokedex</h1>
+			<div className="flex flex-col justify-between">
+				<div className="w-[800px] h-[550px] bg-[url('https://shots.codepen.io/Negor/pen/JEXmMJ-800.jpg?version=1488491989')] bg-contain bg-no-repeat"></div>
 			</div>
 		</div>
 	);
