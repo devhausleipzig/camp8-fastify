75d1a09b63f8d7ec9eebb45565add68f3468d212
Franz Wollang <franzwollang@gmail.com>
2023-01-24 11:25:14 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,53 @@ const exampleDB = {
 };
 
 // Declare a route/endpoint
-fastify.get("/pokemon", async (request, reply) => {});
+fastify.get("/pokemon", async (request, reply) => {
+	return exampleDB.pokemon;
+});
 
-fastify.post("/pokemon", async (request, reply) => {});
+fastify.post("/pokemon", async (request, reply) => {
+	const pokemon = request.body;
 
-fastify.put("/pokemon", async (request, reply) => {});
+	//@ts-ignore
+	exampleDB.pokemon.push(pokemon);
+});
+
+type Pokemon = {
+	name: string;
+	hp: number;
+	attribute1: string;
+	attribute2: string;
+};
+
+fastify.put("/pokemon/:name", async (request, reply) => {
+	//@ts-ignore
+	const { name } = request.params;
+	console.log(name);
+	const newPokemon = request.body as Pokemon;
+
+	const pokemon = exampleDB.pokemon.find((pokemon) => {
+		return pokemon.name == name;
+	});
+
+	if (!pokemon) {
+		reply.status(404);
+		reply.send(`Could not find any pokemon called ${name}.`);
+		return;
+	}
+
+	const list = exampleDB.pokemon.filter((pokemon) => {
+		return !(pokemon.name == name);
+	});
+
+	list.push({
+		...newPokemon,
+		name: name
+	});
+
+	exampleDB.pokemon = list;
+
+	reply.status(200);
+});
 
 fastify.patch("/pokemon", async (request, reply) => {});
 
